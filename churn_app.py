a111f20e5f744218b30a0ef9e47df2ceccfa4bc4
Maksim <maxapple@yandex.ru>
2023-10-09 03:52:49 +0300

    local model serving added

---
+++ churn_app.py
@@ -0,0 +1,113 @@
+# coding: utf-8
+"""Приложение Fast API для модели оттока."""
+
+from fastapi import FastAPI, Body
+from typing import Dict, Union
+from pydantic import BaseModel, Field
+
+from fastapi_handler import FastApiHandler
+
+
+class Item(BaseModel):
+    user_id: str
+    params: dict
+
+
+# Создаем приложение Fast API
+app = FastAPI(
+    title="Churn Model API",
+    description="API сервиса получения вероятности оттока",
+    version="1.0.0",
+    docs_url="/api/public/v1/doc"
+)
+
+# Создаем обработчик запросов для API
+app.handler = FastApiHandler()
+
+
+class ItemResponse(BaseModel):
+    """Ответ для предсказания оттока в Churn Model API."""
+    user_id: str = Field(
+        ...,
+        description="Идентификатор пользователя"
+    )
+    probability: float = Field(
+        ...,
+        description="Вероятность оттока"
+    )
+    is_churn: int = Field(
+        ...,
+        description="Принадлежность оттоку"
+    )
+
+
+class ErrorResponse(BaseModel):
+    """Модель ответа для ошибок в Churn Model API."""
+    error: Union[str, Dict] = Field(
+        ...,
+        description="Текст сообщения с ошибкой"
+    )
+
+
+ERROR_RESPONSES = {
+    400: {'model': ErrorResponse},
+    422: {'model': ErrorResponse},
+    500: {'model': ErrorResponse}
+}
+
+
+@app.post(
+    "/api/public/v1/churn/",
+    response_model=ItemResponse,
+    responses=ERROR_RESPONSES,
+    include_in_schema=True,
+    summary="Получить предсказание оттока для пользователя.",
+    description="Получить предсказание оттока для пользователя. "
+                "В теле запроса должны быть отправлены параметры для модели."
+)
+async def get_prediction_for_item(
+    user_id: str,
+    params: dict = Body(
+        None,
+        title="Параметры пользователя",
+        description="Параметры пользователя пользователя в формате JSON",
+        example={
+            'gender': 1.0,
+            'SeniorCitizen': 0.0,
+            'Partner': 0.0,
+            'Dependents': 0.0,
+            'Type': 0.5501916796819537,
+            'PaperlessBilling': 1.0,
+            'PaymentMethod': 0.2192247621752094,
+            'MonthlyCharges': 50.8,
+            'TotalCharges': 288.05,
+            'MultipleLines': 0.0,
+            'InternetService': 0.3437455629703251,
+            'OnlineSecurity': 0.0,
+            'OnlineBackup': 0.0,
+            'DeviceProtection': 0.0,
+            'TechSupport': 1.0,
+            'StreamingTV': 0.0,
+            'StreamingMovies': 0.0,
+            'days': 245.0,
+            'services': 2.0
+        }
+    )
+):
+    """Метод для получения вероятности оттока пользователя.
+
+    Args:
+        user_id (str): Идентификатор пользователя.
+        params (dict): Произвольное тело запроса в формате JSON.
+
+    Returns:
+        dict: Полученная рекомендация сетов цен.
+    """
+    print(f"user_id: {user_id}")
+    print(f"params: {params}")
+    all_params = {
+        "user_id": user_id,
+        "params": params
+    }
+    # return {"user_id": user_id, "probability": 0.9, "is_churn": 1}
+    return app.handler.handle(all_params)
---
+++ fastapi_handler.py
@@ -0,0 +1,158 @@
+# coding: utf-8
+"""Класс FastApiHandler, который обрабатывает запросы API."""
+
+
+from fastapi import HTTPException, status
+from catboost import CatBoostClassifier
+
+
+class FastApiHandler:
+    """Класс FastApiHandler, который обрабатывает запрос и возвращает предсказание."""
+
+    def __init__(self):
+        """Инициализация переменных класса."""
+
+        # Типы параметров запроса URI для проверки
+        self.uri_param_types = {
+            "user_id": str,
+            "params": dict
+        }
+
+        self.__load_churn_model(model_path="models/catboost_churn_model.bin")
+
+    def __load_churn_model(self, model_path: str):
+        """Загружаем обученную модель оттока.
+        Args:
+            model_path (str): Путь до модели.
+        """
+        try:
+            self.model = CatBoostClassifier()
+            self.model.load_model(model_path)
+        except Exception as e:
+            print(f"Failed to load model: {e}")
+            self.__raise_server_internal_error()
+
+    def __churn_predict(self, model_params: dict):
+        """Предсказываем вероятность оттока.
+
+        Args:
+            model_params (dict): Параметры для модели.
+
+        Returns:
+            float - вероятность оттока от 0 до 1
+        """
+        return self.model.predict_proba(list(model_params.values()))[1]
+
+    def check_required_uri_params(self, query_params):
+        """Проверяем параметры URI запроса на наличие обязательного набора параметров.
+
+        Args:
+            query_params (dict): Параметры запроса.
+
+        Raise:
+            :class:`fastapi.HTTPException`: если отсутствуют обязательные параметры в URI запроса.
+        """
+
+    def check_required_model_params(self, model_params):
+        """Проверяем параметры пользователя на наличие обязательного набора.
+
+        Args:
+            model_params (dict): Параметры пользователя для предсказания.
+
+        Raise:
+            :class:`fastapi.HTTPException`: если отсутствуют обязательные параметры в meta запроса.
+        """
+
+    def get_invalid_param_types(self, param_types, params_to_check):
+        """Метод для проверки корректности типов значений параметров в словаре.
+
+        Args:
+            param_types (dict): Словарь, в котором ключ - это имя параметра, а значение - его разрешенный тип.
+            params_to_check (dict): Словарь, для которого необходимо сделать проверку корректности типов значений
+            параметров.
+
+        Returns:
+            list: Список названий параметров с неправильными типами. Если таких нет, то возвращается пустой список.
+        """
+        params_with_invalid_types = []
+        for param in params_to_check.keys():
+            if param in param_types:
+                try:
+                    param_type = param_types[param]
+                    param_type(params_to_check[param])
+                except ValueError:
+                    params_with_invalid_types.append(param)
+        return params_with_invalid_types
+
+    def check_uri_param_types(self, query_params):
+        """Проверяем корректность типов параметров запроса URI.
+        Выбрасывает исключение :class:`util.custom_exceptions.InvalidUriParameterTypesError` для найденных параметров,
+        которые имеют неожиданный тип.
+
+        Args:
+            query_params (dict): Параметры запроса.
+
+        Raises:
+            :class:`fastapi.HTTPException`: если имеются параметры с типом, отличным от ожидаемого.
+        """
+        params_with_invalid_types = self.get_invalid_param_types(self.uri_param_types, query_params)
+        if params_with_invalid_types:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"Invalid types of URI parameters: {params_with_invalid_types}"
+            )
+
+    def prepare_params(self, params):
+        """Разбираем запрос и проверяем его корректность.
+
+        Args:
+            params (dict): Словарь параметров запроса.
+
+        Returns:
+            - **dict**: Cловарь со всеми параметрами запроса.
+
+        Raises:
+            :class:`fastapi.HTTPException`: если будут ошибки.
+        """
+        return params
+
+    def __raise_server_internal_error(self):
+        """Вспомогательный метод для генерации 500-й ошибки."""
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Server internal error"
+        )
+
+    def handle(self, params):
+        """Метод для обработки запросов API для уже разобранного и провалидированного словаря параметров запроса.
+
+        Args:
+            params (dict): Словарь параметров запроса.
+
+        Returns:
+            dict: Словарь, содержащий результат выполнения запроса.
+        """
+
+        try:
+            params = self.prepare_params(params)
+        except HTTPException as e:
+            raise e
+        except Exception as e:
+            print(f"Exception while preparing parameters from query: {e}")
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Invalid query"
+            )
+
+        try:
+            model_params = params["params"]
+            user_id = params["user_id"]
+            print(f"Predicting for user_id: {user_id} and params:\n{model_params}")
+            probability = self.__churn_predict(model_params)
+            response = {"user_id": user_id, "probability": probability, "is_churn": int(probability > 0.5)}
+
+        except Exception as e:
+            print(f"Error while handling request: {e}")
+            self.__raise_server_internal_error()
+        else:
+            return response
