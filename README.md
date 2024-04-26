6316166402210aceb85579a598934bccb43ce7b5
denis-42ds <zaytseff.denis@gmail.com>
2024-04-26 11:03:06 +0300

    readme and main.py changed

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# mle-sprint3
-Репозиторий с авторским решением третьего спринта
+# Название проекта: mle-sprint3
+
+## Статус проекта: в работе
+
+## Описание рабочих файлов:
+- [requirements.txt](https://github.com/denis-42ds/mle_sprint3/blob/development/requirements.txt) - список необходимых библиотек для написания FastAPI-приложения;
+- [models](https://github.com/denis-42ds/mle_sprint3/blob/development/research_class.py) - модель оттока;
+- [app](https://github.com/denis-42ds/mle_sprint3/tree/development/app) - директория  с приложением;
+- [data]() - данные, которые модель использует для предсказания;
+- 
+## Установка зависимостей и запуск проекта
+```Bash
+git clone https://github.com/denis-42ds/mle_sprint3.git
+cd mle_sprint3
+pip install -r requirements.txt
+```
--- app/main.py
+++ app/main.py
@@ -13,9 +13,9 @@ def read_root():
     return {"Hello": "World"}
 
 # обработка GET-запросов к URL /service-status
-@app.get("/service-status")
-def health_check():
-    return {"status": "ok"}
+# @app.get("/service-status")
+# def health_check():
+#     return {"status": "ok"}
 
 # обрабатываем запросы к специальному пути для получения предсказания модели
 # предсказание пока что в виде заглушки со случайной генерацией score
@@ -25,11 +25,11 @@ def get_prediction_for_item(user_id: str):
 
 # обрабатываем GET-запросы по пути /v1/credit/{client_id} для получения 
 # ответ выдать или нет кредит на основании кредитного score клиента
-@app.get("/api/credit/{client_id}")
-def is_credit_approved(client_id: str):
-    score = random.random()
-    if score > 0.8:
-        approved = 1
-    else:
-        approved = 0
-    return {"client_id": client_id, "approved": approved}
+# @app.get("/api/credit/{client_id}")
+# def is_credit_approved(client_id: str):
+#     score = random.random()
+#     if score > 0.8:
+#         approved = 1
+#     else:
+#         approved = 0
+#     return {"client_id": client_id, "approved": approved}
